adc693bc89bd3f8138f8c5a4c235ac1d8bc0236d
roman_harazha <roman.harazha@gmail.com>
2022-06-20 18:09:02 +0300

    add sender field to tx

---
+++ modules/consensus/handle_msg.go
@@ -0,0 +1,20 @@
+package consensus
+
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	juno "github.com/forbole/juno/v3/types"
+)
+
+func (m *Module) HandleMsg(index int, msg sdk.Msg, tx *juno.Tx) error {
+	if len(tx.Logs) == 0 {
+		return nil
+	}
+
+	err := m.db.SetTxSender(tx)
+	if err != nil {
+		return fmt.Errorf("error while setting tx senders: %s", err)
+	}
+
+	return nil
+}
